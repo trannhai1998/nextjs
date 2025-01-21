317ef37df7c3c4d55bfbb61a33603b111a0c217f
trannamhai98 <htran@atomiton.com>
2025-01-21 14:38:20 +0700

    http init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		extends: ['next/core-web-vitals', 'next/typescript'],
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'prettier/prettier': [
+				'warn',
+				{
+					arrowParens: 'always',
+					semi: false,
+					trailingComma: 'none',
+					tabWidth: 2,
+					endOfLine: 'auto',
+					useTabs: false,
+					singleQuote: true,
+					printWidth: 120,
+					jsxSingleQuote: true,
+				},
+			],
+		},
+	}),
 ];
 
 export default eslintConfig;
